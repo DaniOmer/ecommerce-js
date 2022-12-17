3d9131b6220a9c1d7ea4996cb7419cbd72ca307c
Omer Dotchamou <ddaniomer@gmail.com>
2022-12-17 20:40:20 +0100

    Ajout d'un produit dans le panier avec saveBasket() getBasket() et addProductToBasket()

--- front/js/product.js
+++ front/js/product.js
@@ -37,6 +37,60 @@ let injectHtml = (data) => {
 }
 
 
+// ************** Ajouter des produits dans le panier ********************** //
 
+// Sauvegarder un panier dans localStorage
+let saveBasket = function (basket){
+	localStorage.setItem("basket", JSON.stringify(basket))
+}
+
+// Récupérer un panier depuis localStorage
+let getBasket = function (){
+	let basket = localStorage.getItem("basket")
+	// Vérifions si un panier a déja été sauvegarder
+	if(basket === null){
+		return []
+	}else{
+		return JSON.parse(basket)
+	}
+}
+
+
+// Ajouter un produit dans le panier
+let addProductToBasket = function (){
+	// searchParams
+	let idProduct = new URL(window.location.href).searchParams.get('id')
+	// Produit ajouté à un panier
+	
+	let cartProduct = {
+		'_id': idProduct,
+		'color': document.getElementById('colors').value,
+		'qty': parseInt(document.querySelector("#quantity").value, 10)
+	}
+
+	// Récupération du panier depuis localstorage
+	let basket = getBasket()
+	// Vérifions si le produit existe dans le panier
+	let foundProduct = basket.find(p => p._id == cartProduct._id && p.color == cartProduct.color)
+	if (foundProduct !== undefined){
+		if((foundProduct.qty + cartProduct.qty) <= 100){
+			foundProduct.qty += cartProduct.qty
+		}
+	}else{
+		if(cartProduct.qty > 0 && cartProduct.qty <= 100 && cartProduct.color != ""){
+			basket.push(cartProduct)
+		}
+	}
+	saveBasket(basket)
+}
+
+
+
+// Envoie des données au click de l'utilisateur
+let sendCart = document.querySelector('#addToCart')
+sendCart.addEventListener('click', (event) =>{
+	event.preventDefault()
+	addProductToBasket()
+})
 
 fetchProduct()
