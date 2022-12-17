b6885f9d206af6ecbbe398c823ce2214b48eb190
Omer Dotchamou <ddaniomer@gmail.com>
2022-12-17 20:36:34 +0100

    Méthode injectHTML() qui récupère les infos d'un produit pour affichage dans le DOM

--- front/js/product.js
+++ front/js/product.js
@@ -9,6 +9,34 @@ async function fetchProduct() {
         .catch(error => {throw new Error(error)})
 }
 
+// *** Afficher les informations du produit sur la page produit
+let injectHtml = (data) => {
+    // Sélecteurs
+    let productName = document.getElementsByTagName('title')
+    let image = document.querySelector('.item__img')
+    let title = document.getElementById('title')
+    let price = document.getElementById('price')
+    let description = document.getElementById('description')
+    let colors = document.getElementById('colors')
+    let img = document.createElement('img')
+
+    // Affectations
+    productName.textContent = data.name
+    img.src = data.imageUrl
+    img.alt = data.altTxt
+    image.appendChild(img)
+    title.textContent = data.name
+    price.textContent = data.price
+    description.textContent = data.description
+    for (let i in data.colors) {
+        const option = document.createElement('option')
+        option.value = data.colors[i]
+        option.text = data.colors[i]
+        colors.add(option)
+    }
+}
+
+
 
 
 fetchProduct()
